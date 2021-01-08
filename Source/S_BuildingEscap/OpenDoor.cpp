ee8f8f4e379b6b9c0780bf4e82c74673ae8085da
github-ocean <62124680+github-ocean@users.noreply.github.com>
2021-01-08 16:00:16 +0530

    Lecture 38

--- Source/S_BuildingEscap/OpenDoor.cpp
+++ Source/S_BuildingEscap/OpenDoor.cpp
@@ -1,11 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
 #include "GameFrameWork/PlayerController.h"
 #include "Engine/TriggerVolume.h"
 #include "Engine/World.h"
-#include "OpenDoor.h"
 
 // Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
